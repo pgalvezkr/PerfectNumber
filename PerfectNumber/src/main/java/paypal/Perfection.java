fbd5b4655f1fe26a18fc0ced8056791de2e593bb
Estefania Galvez <egalvez@cobiscorp.com>
2022-05-13 09:27:02 -0500

    First version of code
    First version of code

---
+++ PerfectNumber/src/main/java/paypal/Perfection.java
@@ -0,0 +1,43 @@
+package main.java.paypal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Perfection {
+	 private static Perfection perf;
+
+	    public synchronized static Perfection getPerf() {
+	        if (perf == null) {
+	            perf = new Perfection();
+	        }
+	        return perf;
+	    }
+
+
+	    public static boolean isPerfect(long candidate) {
+	        boolean retVal;
+	        long[] divisors = GetDivisors(candidate);
+	        int sum = 0;
+	        for (int d = 0; d < 1000; d++)
+	        {
+	            sum = sum + divisors[d];
+	        }
+	        if (sum == candidate)
+	            retVal = true;
+	        return retVal;
+	    }
+
+
+	    private static long[] GetDivisors(long candidate) {
+	        long[] divisors = new long[];
+	        int d = 0;
+	        for (long i = 0; i < candidate; i++) {
+	            long foo = candidate / i;
+	            if (foo * i == candidate) {
+	                divisors[d] = i;
+	                d = d + 1;
+	            }
+	        }
+	        return divisors;
+	    }
+} 
